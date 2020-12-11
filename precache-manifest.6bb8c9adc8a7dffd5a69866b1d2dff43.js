6c981734dfc488571bb21e1a4cedfb7d6f334dd8
jiang-N <yeah_20@163.com>
2020-12-11 16:37:58 +0800

    deploy

---
+++ precache-manifest.6bb8c9adc8a7dffd5a69866b1d2dff43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ade1675fe63c7fc2d23",
+    "url": "/bills-website/css/app.908cac66.css"
+  },
+  {
+    "revision": "0ccf54aab3e1634b9211b0720fdd3d2f",
+    "url": "/bills-website/index.html"
+  },
+  {
+    "revision": "0ade1675fe63c7fc2d23",
+    "url": "/bills-website/js/app.8d037afe.js"
+  },
+  {
+    "revision": "f36705beaf7eaf071211",
+    "url": "/bills-website/js/chunk-vendors.9b90f908.js"
+  },
+  {
+    "revision": "1f134956c66d295d990604debf55d6fe",
+    "url": "/bills-website/manifest.json"
+  },
+  {
+    "revision": "e1d725bf33866f9286d30ceff5c573ff",
+    "url": "/bills-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bills-website/robots.txt"
+  }
+]);
